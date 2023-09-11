c410919cc7e6ef87a09940411f014b397245e85e
Lakshay-Bindal <lakshaybindal999@gmail.com>
2023-09-12 00:45:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+<div>
+  <div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <h1 class="carousel-heading">Skills</h1>
+
+        <ul>
+          <li>Programing Language</li>
+          C++
+          <br />C<li>Web Development</li>
+          HTML5
+          <br />
+          CSS
+          <br />
+          Javascript
+        </ul>
+      </div>
+      <div class="carousel-item">
+        <h1 class="carousel-heading">Education</h1>
+
+        <ul>
+          <li>Btech</li>
+          Thapar Institute of Engineering and Technology
+          <br />
+          Computer Science and Business System
+          <li>Senior Secondary High School</li>
+          Vivekanand Vidya Niketan School(2020-22)
+          <li>High School</li>
+          Nav Durga Public School(2010-20)
+        </ul>
+      </div>
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExample"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExample"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</div>;
